e91e11aceda553691d3b4777c3473880a97ffd94
Ant <ananthajit5d@gmail.com>
2024-10-05 00:40:48 -0700

    it works!

--- rockpaper.js
+++ rockpaper.js
@@ -1,6 +1,8 @@
 var items = ["rock", "paper", "scissors"];
+let playcount = 0;
 let humanScore = 0;
 let computerScore = 0;
+let drawScore = 0;
 function getComputerChoice(){
     
     return items[Math.floor(Math.random()*3.0)]
@@ -24,15 +26,26 @@ function playRound(humanChoice, computerChoice) {
     {
         if(humanChoice == computerChoice){
             console.log("draw");
+            drawScore++;
 
     }
     else{
         console.log(`You lose, ${computerChoice} beats ${humanChoice}`)
         computerScore++;
     }
-}
-}
+    playcount++;
+    changeScore();
+    if(playcount >= 5){
+        resetGame(); // reset game and send message
+    }
 
+}
+}
+function changeScore(){
+    const container = document.querySelector('div');
+    container.textContent = `Win: ${humanScore}/${playcount}, Lost: ${computerScore}/${playcount}, Draw: ${drawScore}/${playcount}`;
+    
+}
 
 function playGame(){
     for(i = 0; i < 5; i++){
@@ -47,6 +60,32 @@ function playGame(){
     else{
         console.log("You lost!");
     }
+   
 }
 
-playGame();
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice());
+    })
+})
+
+function resetGame(){
+    if(humanScore > computerScore){
+        alert(`You Win!`);
+    }
+    else if(computerScore < humanScore){
+        alert(`You Lose! You: ${humanScore}, Computer: ${computerScore}`);
+    }
+    else{
+        alert(`How did you get a draw?`);
+    }
+    humanScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+    playcount = 0;
+    changeScore();
+}
+
